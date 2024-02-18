96b14f0eeea2c05de39e3005b6f20ede7fa442f7
Vikas <vikas.r.shukla@gmail.com>
2024-02-18 19:55:05 +0530

    Commit All

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <UserContextProvider>
+        <h1>Hello World</h1>
+        <Login />
+        <Profile />
+
+      </UserContextProvider>
+    </>
+  )
+}
+
+export default App
---
+++ 08minicontext/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React,{useState,useContext} from 'react'
+import UserContext from '../Context/UserContext'
+
+function Login() {
+
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
+
+    const{setUser}=useContext(UserContext)
+
+    const handleSubmit=(e)=>{
+    e.preventDefault()
+    setUser({username,password})
+    }
+  return (
+    <div>
+
+        <h1>Login</h1>
+        <input type='text' 
+        value={username}
+        onChange={(e)=>setUsername(e.target.value)}
+        placeholder='UserName' />
+        <input type='password' 
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+        placeholder='Password' />
+        <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
